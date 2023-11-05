02f65d65c9df4ddb4a2846c38fb8df457867ea91
Hana <aqilahanaw@gmail.com>
2023-11-05 22:27:35 +0700

    init commit

--- tugasM8/spiders/tugasM8_spider.py
+++ tugasM8/spiders/tugasM8_spider.py
@@ -6,17 +6,17 @@ class Tugasm8SpiderSpider(scrapy.Spider):
     # allowed_domains = ["bit.ly"]
     start_urls = ["https://store.playstation.com/en-id/category/05a2d027-cedc-4ac0-abeb-8fc26fec7180/"]
 
-    def parse(self, response):
+    def parse(self, response): # mengambil judul dan harga game
         titles = response.css("span.psw-t-body.psw-c-t-1.psw-t-truncate-2.psw-m-b-2::text").getall()
         prices = response.css("span.psw-m-r-3::text").getall()
 
-        for title, price in zip(titles, prices):
+        for title, price in zip(titles, prices): # masukan ke dictionary satu persatu data yang telah diambil
             yield{
-                "Nama Game" : title.strip(),
+                "Nama Game" : title.strip(), # 
                 "Harga" : price.strip()
             }
 
-        for i in range(2, 17):
+        for i in range(2, 17): # untuk ambil data next page, dimulai dari 2 sampai 16 (ditambah 1 karena range)
             next_url = f"https://store.playstation.com/en-id/category/05a2d027-cedc-4ac0-abeb-8fc26fec7180/{i}"
             yield scrapy.Request(next_url, callback=self.parse)
         
